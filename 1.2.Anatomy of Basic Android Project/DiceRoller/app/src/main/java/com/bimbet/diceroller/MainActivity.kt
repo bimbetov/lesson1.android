b7c91963e0ab2bc1797b5bb1c5c0e3103bb38eca
bimbetov <beimbik-00@mail.ru>
2021-10-07 08:17:43 +0300

    Added homework of the 3rd topic

--- 1.2.Anatomy of Basic Android Project/DiceRoller/app/src/main/java/com/bimbet/diceroller/MainActivity.kt
+++ 1.2.Anatomy of Basic Android Project/DiceRoller/app/src/main/java/com/bimbet/diceroller/MainActivity.kt
@@ -3,30 +3,42 @@ package com.bimbet.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        diceImage = findViewById(R.id.dice_image)
+
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener{ rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.reset_button)
-        countUpButton.setOnClickListener{ reset() }
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener{ reset()}
     }
 
     private fun reset() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = "0"
+        diceImage.setImageResource(R.drawable.empty_dice)
     }
 
     private fun rollDice(){
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        val randomInt = (1..6).random()
-        resultText.text = randomInt.toString()
+        diceImage.setImageResource(getRandomDiceImage())
     }
+
+    private fun getRandomDiceImage() : Int {
+        return when ((1..6).random()){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+    }
+
 }
